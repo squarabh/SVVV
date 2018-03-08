5500dbee981a050100f2260ea98565e64d0c6676
Saurabh <Saurabhsharma.pankaj@gmail.com>
2018-03-08 20:07:20 +0530

    second commit

--- app/src/main/java/com/svvv/by/squarabh/patni/MainActivity.java
+++ app/src/main/java/com/svvv/by/squarabh/patni/MainActivity.java
@@ -1,7 +1,9 @@
 package com.svvv.by.squarabh.patni;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,8 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
+
+            public void CheckInfo(View v ){
+                Intent i = new Intent(MainActivity.this, InfoActivity.class);
+                startActivity(i);
+            }
+
+            public  void Login(View v1){
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+
+            public void SignUp(View v2){
+                Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(i);
+
+
+            }
+
+
+
 }
